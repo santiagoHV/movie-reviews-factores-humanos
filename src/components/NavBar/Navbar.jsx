3dbd6dabe7773c60ca11283a1e5f21e1239fe0cb
kogarashi <brayanarellano1605@gmail.com>
2023-09-15 11:09:31 -0500

    Agrega manejo de sesion
    Introduce React-Redux para el manejo de sesion, para acceder al estado
    se usa useSelector(state => state.auth). Tambien agrega los componentes
    de Login y Logout. Falta implementar la logica para verificar
    credenciales con el Backend

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -3,19 +3,25 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
     return (
         <Navbar className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="/home">Movie Reviews</Navbar.Brand>
+                <Navbar.Brand href="/">Movie Reviews</Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
-                <Navbar.Text>
-                    <a href="#login">Mark Otto</a>
-                </Navbar.Text>
+                    <Navbar.Text>
+                        {isAuthenticated ? (
+                            <a href="/logout">Logout</a>
+                        ) : (
+                            <a href="/login">Login</a>
+                        )}
+                    </Navbar.Text>
                 </Navbar.Collapse>
+
             </Container>
         </Navbar>
     )
