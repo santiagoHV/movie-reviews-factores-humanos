767d0d6c0c3d7b906ea0fe01b2779d5603580366
kogarashi <brayanarellano1605@gmail.com>
2023-09-16 08:43:45 -0500

    Estilos para la notificacion, tanto mobile como desktop

--- src/components/AlertComponent/AlertComponent.jsx
+++ src/components/AlertComponent/AlertComponent.jsx
@@ -2,15 +2,19 @@ import React, { useEffect } from "react";
 import { Alert } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { hideAlert } from "../../reducers/notificationSlice";
+import './AlertComponent.css'
 
 const AlertComponent = () => {
+    // Obtiene las propiedades del estado de notificación
     const { show, style, message } = useSelector(state => state.notification)
     const dispatch = useDispatch();
 
+    // Maneja el cierre de la alerta
     const handleClose = () => {
         dispatch(hideAlert());
     };
 
+    // Efecto secundario para cerrar la alerta después de 1 segundo
     useEffect(() => {
         if (show) {
             const timeoutId = setTimeout(() => {
@@ -20,11 +24,16 @@ const AlertComponent = () => {
         }
     }, [show, dispatch]);
 
-    return (
-        <Alert variant={style} show={show} onClose={handleClose} dismissible style={{bottom: '0'}}>
-            {message}
-        </Alert>
-    )
+    // Renderiza la alerta solo si show es verdadero
+    const alertContainer = show ? (
+        <div className="alertContainer">
+            <Alert variant={style} show={show}>
+                {message}
+            </Alert>
+        </div>
+    ) : null
+
+    return alertContainer
 };
 
 export default AlertComponent;
