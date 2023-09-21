6a597b817f689254706833f1c2eec035fa4cc53d
kogarashi <brayanarellano1605@gmail.com>
2023-09-21 12:08:35 -0500

    Carrusel de peliculas para la pagina principal

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import CarouselMovie from '../../components/CarouselMovie/CarouselMovie';
 import CardMovie from "../../components/CardMovie/CardMovie";
 import Badge from 'react-bootstrap/Badge';
+import CarouselCard from "../../components/CarouselCard/CarouselCard";
 
 const Home = () => {
     const movies = [
-        {   'image': 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2019/04/mad-max-furia-carretera.jpg?tf=3840x', 
+        {
+            'image': 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2019/04/mad-max-furia-carretera.jpg?tf=3840x',
             'title': 'Nombre Pelicula xd',
-            'description':'lorem imsum'
+            'description': 'lorem imsum'
         },
-        {   'image': 'https://static.posters.cz/image/hp/79858.jpg', 
+        {
+            'image': 'https://static.posters.cz/image/hp/79858.jpg',
             'title': 'Pelicula Harry potter',
-            'description':'deSCRIPCION 2'
+            'description': 'deSCRIPCION 2'
         }
     ]
 
@@ -20,9 +23,10 @@ const Home = () => {
             <h2>Agregado recientemente <Badge bg="secondary">New</Badge></h2>
             <CarouselMovie movies={movies} />
             <h2>Recomendado para ti</h2>
+            <CarouselCard movies={movies} size={5} />
             <div>
                 <h2>Terror</h2>
-                <CardMovie />
+                <CarouselCard movies={movies} size={5} />
             </div>
         </div>
     )
