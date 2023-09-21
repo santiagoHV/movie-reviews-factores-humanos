c6f43c42f07216d247f88322eabb2ac54f92428b
kogarashi <brayanarellano1605@gmail.com>
2023-09-21 14:43:43 -0500

    Estilos de tarjetas

--- src/components/CardMovie/CardMovie.jsx
+++ src/components/CardMovie/CardMovie.jsx
@@ -1,17 +1,18 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './CardMovie.css'
 
-function CardMovie({title, image, description}) {
+function CardMovie({ title, image, description }) {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image} />
-      <Card.Body>
+    <Card className="cardMovie">
+      <Card.Img src={image} />
+      <Card.ImgOverlay>
         <Card.Title>{title}</Card.Title>
         <Card.Text>
           {description}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
+        <Button variant="primary">Opinar</Button>
+      </Card.ImgOverlay>
     </Card>
   );
 }
